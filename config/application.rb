aae7920b82d55c63e9f4ddced9f1bc5d1056ad1b
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-16 12:10:02 -0800

    build(config): WIP, troubleshooting app & test environment config
    Following suggestions in errors, warnings that come up after running specs.
    These are some consequences of upgrading from Rails 4.2.5.1 to 5.0.0.1

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module JobTracker
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Flash
