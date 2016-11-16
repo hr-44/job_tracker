8fa910f29e1f94706c4cd6f92bb8a439cc75aa89
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-16 14:29:05 -0800

    fix(config/routes): Specify routes for #new, #edit actions
    The generated routes were not coming up as they were before. There have
    not been any major changes to the routing file in a long time. Specs
    have always passed. Something in Rails 5 makes it generate routes
    differently than expected. So I've specified the routes that are
    breaking tests. They pass now, though the file is pretty messy-looking.

--- config/routes.rb
+++ config/routes.rb
@@ -14,22 +14,44 @@ Rails.application.routes.draw do
   # User accounts, profiles
   get '/register', to: 'users#new'
   resource :profile, controller: 'users', as: 'user', except: :new
+  get '/profile/edit', to: 'users#edit'
 
   resources :notes,         only: :index
   resources :cover_letters, only: :index, controller: 'job_applications/cover_letters'
   resources :postings,      only: :index, controller: 'job_applications/postings'
+
+  get '/companies/new', to: 'companies#new'
+  get '/companies/:id/edit', to: 'companies#edit'
+
   resources :companies,     except: :destroy do
     resources :recruitments, controller: 'recruitments', only: [:new, :create, :destroy]
   end
   resources :categories,    only: :show
 
+  get '/job_applications/new', to: 'job_applications#new'
+  get '/job_applications/:id/edit', to: 'job_applications#edit'
+
   resources :job_applications do
+    get '/notes/new',   to: 'notes#new'
+    get '/notes/:id/edit',  to: 'notes#edit'
+
     resources :notes,         except: :index
     resource  :cover_letter,  controller: 'job_applications/cover_letters'
     resource  :posting,       controller: 'job_applications/postings'
+
+    get '/cover_letter/new',  to: 'job_applications/cover_letters#new'
+    get '/cover_letter/edit', to: 'job_applications/cover_letters#edit'
+    get '/posting/new',  to: 'job_applications/postings#new'
+    get '/posting/edit', to: 'job_applications/postings#edit'
   end
 
+  get 'contacts/:id/edit', to: 'contacts#edit'
+  get 'contacts/new', to: 'contacts#new'
+
   resources :contacts do
+    get '/notes/new',   to: 'notes#new'
+    get '/notes/:id/edit',  to: 'notes#edit'
+
     resources :notes, except: :index
   end
 end
