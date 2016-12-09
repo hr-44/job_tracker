76b60f5efeecbb5a47300eeed6db2515e89c33cf
Rafael Espinoza <nerddiffer@gmail.com>
2016-12-08 20:23:23 -0800

    fix(app/controllers): Include 3 ActionController modules
    Temporarily need them, while sorting out transition to API-only app.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::API
   include AbstractController::Helpers
   include ActionController::RequestForgeryProtection
+  include ActionController::MimeResponds
+  include ActionController::Cookies
+
+  # TODO: remove all flash-related code from controllers
+  include ActionController::Flash
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
