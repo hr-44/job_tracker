ed8f70ce18ca068fa037e67978cf25de872a9a05
Rafael Espinoza <nerddiffer@gmail.com>
2016-12-14 21:53:36 -0800

    refactor(app/controllers): Refactor TokenController
    refactor(app/controllers): Move params to private method
    
    feat(app/controllers): Respond with 400 if missing some params
    
    refactor(app/controllers): Clean up TokenController
    - Move param checking to make for more effective testing.
    - Use attr_reader for same reason.
    
    fix(app/controllers): Set correct ivars
    
    refactor(token): Shorten error message
    - The built-in message does most of what's needed

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,24 +1,41 @@
 class TokenController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :handle_missing_params
+
+  attr_reader :auth_token
+
   skip_before_action :authorize_request
+  before_action :authenticate_user
 
   def create
-    result = authenticate_user(email: params[:email], password: params[:password])
-
-    if !result
+    if !auth_token
       render('auth/errors', status: :unauthorized)
     else
-      @auth_token = result
       render('auth/token', status: :ok)
     end
   end
 
   private
 
-  def authenticate_user(email:, password:)
+  def check_params
+    raise ActionController::ParameterMissing.new('email') unless params[:email].present?
+    raise ActionController::ParameterMissing.new('password') unless params[:password].present?
+  end
+
+  def handle_missing_params(e)
+    @errors = e
+    render('shared/errors', status: :bad_request)
+  end
+
+  def authenticate_user
+    check_params
+
+    email = params[:email]
+    password = params[:password]
+
     user = User.find_by_email(email)
 
     if user && user.authenticate(password)
-      JsonWebToken.encode(user_id: user.id)
+      @auth_token = JsonWebToken.encode(user_id: user.id)
     else
       add_auth_error!('Credentials invalid')
       nil
