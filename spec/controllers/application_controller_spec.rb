c34fa929f6dc08a7620052d361ae7062dd1c4042
Rafael Espinoza <nerddiffer@gmail.com>
2016-12-15 12:55:08 -0800

    test(controllers): Test AuthorizationHelper
    Nesting tests inside of ApplicationController because we need a
    controller context. Same approach as testing SessionsHelper.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,6 +3,212 @@ require 'rails_helper'
 RSpec.describe ApplicationController, type: :controller do
   let(:user) { build(:user) }
 
+  context 'AuthorizationHelper' do
+    let(:good_headers) { { 'Authorization' => 'Basic deadbeef' } }
+    let(:bad_headers)  { { 'foo' => 'bar' } }
+
+    describe '#authorize_api_request' do
+      context 'when token is decoded' do
+        before(:each) do
+          allow(controller).to receive(:decode_auth_token).with(good_headers).and_return('ok')
+        end
+
+        shared_examples_for 'expected method calls for #authorize_api_request' do
+          after(:each) do
+            controller.send(:authorize_api_request, good_headers)
+          end
+
+          it 'calls #decode_auth_token' do
+            expect(controller).to receive(:decode_auth_token)
+          end
+          it 'calls #find_user_from_token' do
+            expect(controller).to receive(:find_user_from_token)
+          end
+        end
+
+        context 'when user is found' do
+          before(:each) do
+            allow(controller).to receive(:find_user_from_token).and_return(user)
+          end
+
+          it_behaves_like 'expected method calls for #authorize_api_request'
+
+          it 'returns the user' do
+            actual = controller.send(:authorize_api_request, good_headers)
+            expect(actual).to eq(user)
+          end
+          it 'does not add any @auth_errors' do
+            controller.send(:authorize_api_request, good_headers)
+            expect(assigns(:auth_errors)).to be_nil
+          end
+        end
+
+        context 'when user is not found' do
+          before(:each) do
+            allow(controller).to receive(:find_user_from_token).and_return(nil)
+          end
+
+          it_behaves_like 'expected method calls for #authorize_api_request'
+
+          it 'returns nil' do
+            actual = controller.send(:authorize_api_request, good_headers)
+            expect(actual).to be_nil
+          end
+          it 'adds errors to @auth_errors' do
+            controller.send(:authorize_api_request, good_headers)
+            expect(assigns(:auth_errors)).not_to be_nil
+          end
+        end
+      end
+
+      context 'when token is not decoded' do
+        before(:each) do
+          allow(controller).to receive(:decode_auth_token).with(bad_headers).and_return(nil)
+        end
+
+        it 'adds a value to @auth_errors' do
+          controller.send(:authorize_api_request, bad_headers)
+          expect(assigns(:auth_errors)).not_to be_nil
+        end
+        it 'returns nil' do
+          actual = controller.send(:authorize_api_request, bad_headers)
+          expect(actual).to be_nil
+        end
+      end
+    end
+
+    describe '#decode_auth_token' do
+      shared_examples_for 'expected method calls for #decode_auth_token' do
+        it 'calls #http_auth_header' do
+          expect(controller).to receive(:http_auth_header)
+        end
+        it 'calls .decode on JsonWebToken' do
+          expect(JsonWebToken).to receive(:decode)
+        end
+      end
+
+      context 'with good headers' do
+        before(:each) do
+          allow(controller).to receive(:http_auth_header).with(:good_headers).and_return('ok')
+          allow(JsonWebToken).to receive(:decode).with('ok').and_return('decoded token')
+        end
+
+        after(:each) do
+          controller.send(:decode_auth_token, :good_headers)
+        end
+
+        it_behaves_like 'expected method calls for #decode_auth_token'
+
+        it 'returns a decoded token' do
+          actual = controller.send(:decode_auth_token, :good_headers)
+          expect(actual).to eq 'decoded token'
+        end
+      end
+
+      context 'with bad headers'  do
+        before(:each) do
+          allow(controller).to receive(:http_auth_header).with(:bad_headers).and_return(nil)
+          allow(JsonWebToken).to receive(:decode).with(nil).and_return(nil)
+        end
+
+        after(:each) do
+          controller.send(:decode_auth_token, :bad_headers)
+        end
+
+        it_behaves_like 'expected method calls for #decode_auth_token'
+
+        it 'returns something falsey' do
+          actual = controller.send(:decode_auth_token, :bad_headers)
+          expect(actual).to be_falsey
+        end
+      end
+    end
+
+    describe '#http_auth_header' do
+      context 'when Authorization header is present' do
+        it 'returns an encoded token' do
+          actual = controller.send(:http_auth_header, good_headers)
+          expect(actual).to eq 'deadbeef'
+        end
+      end
+
+      context 'when Authorization header is not present' do
+        it 'adds value to @auth_errors' do
+          controller.send(:http_auth_header, bad_headers)
+          expect(assigns(:auth_errors)).not_to be_nil
+        end
+        it 'returns nil' do
+          actual = controller.send(:http_auth_header, bad_headers)
+          expect(actual).to be_nil
+        end
+      end
+    end
+
+    describe '#find_user_from_token' do
+      before(:each) do
+        allow(user).to receive(:id).and_return(1)
+        allow(User).to receive(:find).with(1).and_return(user)
+        allow(User).to receive(:find).with(2).and_return(nil)
+      end
+
+      context 'when user is found' do
+        let(:token) { { user_id: 1 } }
+
+        it 'calls .find on User' do
+          expect(User).to receive(:find).with(1)
+          controller.send(:find_user_from_token, token)
+        end
+
+        it 'returns the user with matching id' do
+          actual = controller.send(:find_user_from_token, token)
+          expect(actual).to eq(user)
+          expect(actual.id).to be(1)
+        end
+      end
+
+      context 'when user is not found' do
+        let(:token) { { user_id: 2 } }
+
+        it 'calls .find on User' do
+          expect(User).to receive(:find).with(2)
+          controller.send(:find_user_from_token, token)
+        end
+
+        it 'returns nil' do
+          actual = controller.send(:find_user_from_token, token)
+          expect(actual).to be_nil
+        end
+      end
+    end
+
+    describe '#add_auth_error!' do
+      let(:message) { 'bar' }
+
+      context 'when @auth_errors is nil' do
+        before(:each) do
+          controller.instance_eval { @auth_errors = nil }
+        end
+
+        it 'sets @auth_errors to an array' do
+          expect{ controller.send(:add_auth_error!, message) }
+            .to change { assigns(:auth_errors) }.from(nil).to([message])
+        end
+      end
+
+      context 'when @auth_errors already present' do
+        before(:each) do
+          controller.instance_eval { @auth_errors = ['foo'] }
+        end
+
+        it 'adds a message to @auth_errors' do
+          expect{ controller.send(:add_auth_error!, message) }
+            .to change { assigns(:auth_errors) }
+            .from(['foo']).to(['foo', message])
+        end
+      end
+    end
+  end
+
   # TODO: figure out cookies
   xdescribe '#current_user' do
     context 'when session has a user_id' do
